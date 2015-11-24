eea9846daeac43503c91df7ea83992c7daf0aeb8
Hdavid <hajnal.david@erise.hu>
2015-11-24 20:47:55 +0100

    Basic rucsify

--- dist/rucsok.js
+++ dist/rucsok.js
@@ -21,11 +21,91 @@
 
 })(window)
 
-(function(rucsok){
+;(function(rucsok){
 
-  rucsok[(!![]+[])[+!![]]+([][[]]+[])[+[]]+([]+{})[!+[]+!![]+!![]+!![]+!![]]+(![]+[])[!+[]+!![]+!![]]+([][[]]+[])[!+[]+!![]+!![]+!![]+!![]]+([][[]]+[])[!+[]+!![]+!![]+!![]]+(+(+!![]+([][[]]+[])[!+[]+!![]+!![]]+(+!![]+[])+(+[]+[])+(+[]+[])+(+[]+[]))+[])[!+[]+!![]+!![]+!![]+!![]+!![]+!![]]] = function(options){
+  // helper watevers
+  
+  var Maybe = function(value) {
+
+    var Nothing = {
+      bind: function(fn) { 
+        return this; 
+      },
+      isNothing: function() { 
+        return fn.call(this);
+      },
+      val: function() { 
+        throw new Error("cannot call val() nothing"); 
+      },
+      maybe: function(def, fn) {
+        return def;
+      }
+    };
+
+    var Just = function(value) { 
+      return {
+        bind: function(fn) { 
+          return Maybe(fn.call(this, value));
+        },
+        isNothing: function() { 
+          return false; 
+        },
+          val: function() { 
+            return value;
+          },
+          maybe: function(def, fn) {
+            return fn.call(this, value);
+          }
+        };
+    };
+
+   if (typeof value === 'undefined' ||   value === null ||  (typeof value.isNothing !== 'undefined' && value.isNothing()))
+    {
+      return Nothing;
+    }
+
+    return Just(value);
+  };
+
+  function createIframeFromUrl(url){
+    var iframe = document.createElement('iframe');
+    iframe.src = encodeURI(url);
+    return iframe;    
+  }
+
+  // api
+
+  var rucsokFactory = function(elem){
+
+    var r = {};
+
+    r["rucsify"] = function(options){
+      
+      var rucssult = Maybe(options)
+        .bind(function(o){
+          return o['url'];
+        })
+        .maybe("Valamit elszámolhattál!", function(url){         
+          var iframe = createIframeFromUrl(url);         
+          iframe.width = elem.clientWidth;
+          iframe.height = elem.clientHeight;
+          elem.appendChild(iframe); 
+          return "Rücsifed!";
+        });        
+
+       console.log(rucssult);
+    }
+
+    return r;
 
   }
 
-})(window.rucsok || {})
+  // rewrite zsquery
+
+  window.$ = function(id){
+    var elem = document.querySelector(id);
+    return rucsokFactory(elem);
+  }
+
+})(window.rucsok || {});
 
